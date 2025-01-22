5983be9fb62d0a48905a665f7760425e296e7a70
luccasissao <87354096+luccasissao@users.noreply.github.com>
2025-01-22 20:16:13 -0300

    Delete src directory

--- src/data_loader.py
+++
@@ -1,7 +0,0 @@
-import torchaudio
-from torch.utils.data import DataLoader
-
-def load_librispeech(batch_size=8, split='train-clean-100', root='./data'):
-    dataset = torchaudio.datasets.LIBRISPEECH(root=root, url=split, download=True)
-    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    return data_loader
